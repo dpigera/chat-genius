e3093420e901e9f4b82215e8259866637943395b
dpigera <dpigera@gmail.com>
2025-01-07 00:20:23 +0000

    Added login popup view

---
+++ app/components/user-profile-popup.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class UserProfilePopupComponent extends Component {
+  @service session;
+  @service router;
+
+  @action
+  async signOut() {
+    await this.session.invalidate();
+    this.router.transitionTo('login');
+  }
+} 
