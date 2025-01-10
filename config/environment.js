b7d61974a90acd926c4d226fdaa4420ba5ae1b41
dpigera <dpigera@gmail.com>
2025-01-11 01:43:41 +0530

    ENV diff

--- config/environment.js
+++ config/environment.js
@@ -9,14 +9,10 @@ module.exports = function (environment) {
     EmberENV: {
       EXTEND_PROTOTYPES: false,
       FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
     },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
     },
   };
 
@@ -26,12 +22,7 @@ module.exports = function (environment) {
 
   if (environment === 'development') {
     ENV.APP.API_HOST = 'http://localhost:3000';
-    ENV['ember-cli-mirage'] = {
-      enabled: false
-    };
   }
-
-  
 
   if (environment === 'test') {
     ENV.locationType = 'none';
@@ -39,18 +30,10 @@ module.exports = function (environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
-    ENV['ember-cli-mirage'] = {
-      enabled: false
-    };
-
   }
 
   if (environment === 'production') {
-    ENV.APP.API_HOST = 'https://apple-server.fly.dev';
-    ENV['ember-cli-mirage'] = {
-      enabled: false
-    };
-
+    ENV.APP.API_HOST = 'https://gauntlet-chatgenius-pocketbase-2.fly.dev';
   }
 
   return ENV;
